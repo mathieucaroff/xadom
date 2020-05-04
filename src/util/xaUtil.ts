fa002b9b4e84caff61505c308860eaecf3298405
Mathieu CAROFF <mathieu.caroff@free.fr>
2020-05-05 01:40:10 +0200

    src (xaUtil.ts fix) create element types

--- src/util/xaUtil.ts
+++ src/util/xaUtil.ts
@@ -12,23 +12,31 @@ export const createXaUtil = (d: Document) => {
     * @param attribute An object associating keys to values for the created element
     * @param children An array of children elements
     */
-   let create = <
-      K extends keyof (HTMLElementTagNameMap | SVGElementTagNameMap)
-   >(
+   let create = <K extends keyof HTMLElementTagNameMap>(
       name: K,
-      attribute: Record<string, string> = {},
+      attribute: Partial<HTMLElementTagNameMap[K]> & Record<string, any> = {},
       children: Element[] = [],
    ) => {
-      let elem = d.createElement<K>(name as K)
+      // Create element
+      let elem = d.createElement<K>(name)
 
+      // Copy each attribute
       Object.entries(attribute).forEach(([name, value]) => {
          if (elem[name] !== undefined) {
             elem[name] = value
          } else {
+            if (typeof value !== 'string') {
+               console.warn(
+                  `hyper: met unexpected attribute ${name}`,
+                  value,
+                  new Error().stack,
+               )
+            }
             elem.setAttribute(name, value)
          }
       })
 
+      // Insert each child
       children.forEach((child) => {
          elem.appendChild(child)
       })
